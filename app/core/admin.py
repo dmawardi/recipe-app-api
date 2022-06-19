82544cd408b016d8f418e5d295e7ce9cd73322b7
dmawardi <dmawardi@gmail.com>
2022-06-19 10:30:44 +0800

    Added fieldsets, creation fieldsets, readonly fields, and tests for editing user admin section

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # import models
 from core import models
+# import translation system utilities
+from django.utils.translation import gettext_lazy as _
 
 
 # Register your models here.
@@ -12,6 +14,41 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+    # This needs to be added to allow editing of the user
+    # This is because we replaced the default behaviour
+    fieldsets = (
+        # None for title, 2 fields: email & password
+        (None, {"fields": ('email', 'password')}),
+        (
+            # Use Django translations to translate
+            # Permissions for title
+            _('Permissions'), {
+                'fields': ('is_active', 'is_staff', 'is_superuser',)}
+        ),
+        (
+            _('Important Dates'), {
+                'fields': ('last_login',)
+            }
+        )
+    )
+    readonly_fields = ['last_login']
+    # fieldsets for add form
+    # Required changing due to modifications from default
+    add_fieldsets = (
+        (None, {
+            # Add optional css class to make fields slightly more spaced
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -21,7 +21,7 @@ class AdminSiteTests(TestCase):
             email="user@example.com", password="testpass123")
 
     def test_users_list(self):
-        """Tests that users are listed on the page"""
+        """Tests that users are listed on the page using name and email."""
         # build url to test using reverse function and Django location string
         url = reverse("admin:core_user_changelist")
         # Obtain response using client
@@ -29,3 +29,21 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Tests that the edit user page works."""
+        # Use reverse to obtain url required (found by looking in browser)
+        # sample admin url: core/user/:id/change
+        # User
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        # Obtain response using client
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
